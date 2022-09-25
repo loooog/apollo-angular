d50b79c05b5ad7659ad141dc4ff4cf3c5bb99a38
Tuval Simha <tuval.simha@Gmail.com>
2022-09-25 17:38:35 +0300

    Update website/src/pages/_app.tsx
    Co-authored-by: Dimitri POSTOLOV <en3m@ya.ru>

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@theguild/components/style.css';
 
-export default function App({Component, pageProps}: any) {
+export default function App({Component, pageProps}: AppProps) {
   return <Component {...pageProps} />;
 }
