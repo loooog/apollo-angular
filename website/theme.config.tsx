72d2138816a9affa0568c2e5c6df229e62331301
TuvalSimha <tuval.simha@gmail.com>
2022-09-25 16:14:48 +0300

    fix

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,41 +1,67 @@
-import {AngularLogo, DocsThemeConfig} from '@theguild/components';
+/* eslint sort-keys: error */
+import {
+  AngularLogo,
+  FooterExtended,
+  mdxComponents,
+  Header,
+  Navbar,
+} from '@theguild/components';
+import {DocsThemeConfig} from 'nextra-theme-docs';
 
 const SITE_NAME = 'Apollo Angular';
 
 const config: DocsThemeConfig = {
-  titleSuffix: ` – ${SITE_NAME}`,
-  projectLink: 'https://github.com/kamilkisiela/apollo-angular', // GitHub link in the navbar
+  components: mdxComponents,
   docsRepositoryBase:
-    'https://github.com/kamilkisiela/apollo-angular/tree/master/website/src/pages', // base URL for the docs repository
-  nextLinks: true,
-  prevLinks: true,
-  search: false,
-  floatTOC: true,
-  darkMode: true,
-  footer: false,
-  footerEditLink: 'Edit this page on GitHub',
-  logo: (
-    <>
-      <AngularLogo className="mr-1.5 h-9 w-9" />
-      <div>
-        <h1 className="md:text-md text-sm font-medium">{SITE_NAME}</h1>
-        <h2 className="hidden text-xs sm:!block">
-          GraphQL Client for Angular Framework
-        </h2>
-      </div>
-    </>
-  ),
-  head: () => (
+    'https://github.com/kamilkisiela/apollo-angular/tree/master/website/src/pages',
+  editLink: {
+    text: 'Edit this page on GitHub',
+  },
+  feedback: {
+    content: 'Question? Give us feedback →',
+    labels: 'kind/docs',
+  },
+  footer: {
+    component: <FooterExtended />,
+  },
+  head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content={`${SITE_NAME}: documentation`} />
       <meta name="og:title" content={`${SITE_NAME}: documentation`} />
     </>
   ),
-  gitTimestamp: 'Last updated on',
-  defaultMenuCollapsed: true,
-  feedbackLink: 'Question? Give us feedback →',
-  feedbackLabels: 'kind/docs',
+  logo: (
+    <>
+      <AngularLogo className="mr-1.5 h-9 w-9" />
+      <div>
+        <h1 className="md:text-md text-sm font-medium">{SITE_NAME}</h1>
+        <h2 className="hidden text-xs sm:block">
+          GraphQL Client for Angular Framework
+        </h2>
+      </div>
+    </>
+  ),
+  navbar: (props) => (
+    <>
+      <Header
+        accentColor="#1cc8ee"
+        themeSwitch
+        searchBarProps={{version: 'v2'}}
+      />
+      <Navbar {...props} />
+    </>
+  ),
+  project: {
+    link: 'https://github.com/kamilkisiela/apollo-angular',
+  },
+  search: {
+    component: null,
+  },
+  sidebar: {
+    defaultMenuCollapsed: true,
+  },
+  titleSuffix: ` – ${SITE_NAME}`,
 };
 
 export default config;
